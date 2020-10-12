ff8ede0f14b20d29d1c856b1a14ad0022f6887b1
Andrew_Dev <xolluej@list.ru>
2020-10-12 22:59:47 +0400

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,33 @@
+const disableScroll = function() {
+    let paddingOffset = window.innerWidth - document.body.offsetWidth + 'px'
+    document.body.classList.toggle('scroll-lock')
+    document.body.style.paddingRight = paddingOffset
+}
+
+const burger = document.querySelector('.menu-icon').addEventListener('click', function() {
+    const menu = document.querySelector('.menu')
+    this.classList.toggle('menu-icon__active')
+    menu.classList.toggle('menu__active')
+    disableScroll()
+
+    let links = document.querySelectorAll('.menu__link').forEach(element => {
+        element.onclick = () => {
+            menu.classList.toggle('menu__active')
+            this.classList.toggle('menu-icon__active')
+            disableScroll()
+        }
+    })
+})
+
+let time = 3600
+const countDownEl = document.querySelector('.timer')
+
+setInterval(updateCountDown, 1000)
+
+function updateCountDown() {
+    const minutes = Math.floor(time / 60)
+    let seconds = time % 60
+    seconds = seconds < 10 ? '0' + seconds : seconds
+    countDownEl.innerHTML = `${minutes}:${seconds}`
+    time--
+}
